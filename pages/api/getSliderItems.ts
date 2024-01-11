0da5c60913b786afce8de3c8db3a06e87ec830c7
Kwammeeh <dufeemmanuel.code@gmail.com>
2024-01-11 07:21:31 +0000

    error-fixed

--- pages/api/getSliderItems.ts
+++ pages/api/getSliderItems.ts
@@ -1,19 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { groq } from "next-sanity";
 import { sanityClient } from "../../sanity";
-import { Location, sliderItems } from "../../typing";
+import { Location, SliderItems } from "../../typing";
 
 const query = groq`
 *[_type == "sliderItems"]`;
 
 type Data = {
-  sliderItems: sliderItems[];
+  sliderItems: SliderItems[];
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const sliderItems: sliderItems[] = await sanityClient.fetch(query);
+  const sliderItems: SliderItems[] = await sanityClient.fetch(query);
   res.status(200).json({ sliderItems });
 }
